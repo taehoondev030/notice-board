7c22b2c5e83e68dbce0cd7b1deebb92d310de6b3
taehoondev030 <taehoondev030@gmail.com>
2025-05-23 22:50:35 +0900

    Article 엔티티 생성

---
+++ src/main/java/taehoon/board/article/Article.java
@@ -0,0 +1,28 @@
+package taehoon.board.article;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // 생성자 위에 입력 시 빌더 패턴으로 객체 생성 가능
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
