18a138deb352d44b9a21ae3dc972e9c35885fa89
taehoondev030 <taehoondev030@gmail.com>
2025-05-23 23:03:58 +0900

    게시물 조회 구현

--- src/main/java/taehoon/board/article/controller/BoardController.java
+++ src/main/java/taehoon/board/article/controller/BoardController.java
@@ -3,12 +3,16 @@ package taehoon.board.article.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import taehoon.board.article.domain.Article;
 import taehoon.board.article.dto.AddArticleRequest;
+import taehoon.board.article.dto.ArticleResponse;
 import taehoon.board.article.service.BoardService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형태로 반환하는 컨트롤러
@@ -24,4 +28,16 @@ public class BoardController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = boardService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
 }
--- src/main/java/taehoon/board/article/service/BoardService.java
+++ src/main/java/taehoon/board/article/service/BoardService.java
@@ -6,6 +6,8 @@ import taehoon.board.article.domain.Article;
 import taehoon.board.article.dto.AddArticleRequest;
 import taehoon.board.article.repository.BoardRepository;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final, @NotNull이 붙은 필드 생성자 추가
 @Service // 빈으로 등록
 public class BoardService {
@@ -16,6 +18,11 @@ public class BoardService {
     // JpaRepository에서 지원하는 저장(save) 메서드
     public Article save(AddArticleRequest request) {
         return boardRepository.save(request.toEntity());
+    }
+
+    // DB에 저장된 전체 게시물 조회
+    public List<Article> findAll() {
+        return boardRepository.findAll();
     }
 
 }
